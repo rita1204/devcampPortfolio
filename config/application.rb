edf1664665e5722dc307c29cdfb54b7e9f5b5612
rita1204 <pittixichan@gmail.com>
2018-07-18 11:16:16 +0900

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module DevcampPortfolio
   class Application < Rails::Application
     config.load_defaults 5.2
     config.generators.system_tests = nil
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
