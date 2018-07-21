ee9685fad01ea002406a1d7293293ffb0f631887
rita1204 <pittixichan@gmail.com>
2018-07-21 10:47:40 +0900

    Revise style fortech-news page and add tech-news to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,16 +27,16 @@ module ApplicationHelper
         title: 'About me'
       },
       {
-        url: contact_path,
-        title: 'Contact'
-      },
-      {
         url: blogs_path,
         title: 'Blog'
       },
       {
         url: portfolios_path,
         title: 'Portfolio'
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
       }
     ]
   end
