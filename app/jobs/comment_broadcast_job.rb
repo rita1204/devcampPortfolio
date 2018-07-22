898411640c080bcd9aa35a6edbbf95e9954f73a4
rita1204 <pittixichan@gmail.com>
2018-07-22 10:24:23 +0900

    Created a comment broadcast,configured websocket routes and updated the comment model with callback

---
+++ app/jobs/comment_broadcast_job.rb
@@ -0,0 +1,14 @@
+class CommentBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(comment)
+    Actioncable.server.broadcast "blogs_#{comment.blog.id}_channel",comment: render_comment(comment)
+  end
+
+  private 
+
+  def render_comment(comment)
+    CommentController.render partial: 'comments/comment',locals:{comment:comment}
+  end
+
+end
